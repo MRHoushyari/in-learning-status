14aa63545a1c7bbc7277f806804e6b70a72bda96
mohammadreza <MR.Houshyari@Gmail.com>
2022-10-29 15:12:30 +0330

    popestate bug fix

--- JAVA_Learn/State/script.js
+++ JAVA_Learn/State/script.js
@@ -4,32 +4,45 @@ data = {
   text: "HOME",
 };
 document.querySelector("nav").addEventListener("click", (e) => {
-  switch (e.target.getAttribute("itemid")) {
-    case "home":
-      window.history.pushState(data,"", data.itemid);
-      data = { itemid: "home", text: "HOME", color: "red" };
-      update(data);
-      break;
-    case "search":
-      data = { itemid: "search", text: "SEARCH", color: "blue" };
-      window.history.pushState(data,"", data.itemid);
-      update(data);
-      break;
-    case "likes":
-      data = { itemid: "likes", text: "LIKES", color: "pink" };
-      window.history.pushState(data,"", data.itemid);
-      update(data);
-      break;
-    case "profile":
-      data = { itemid: "profile", text: "PROFILE", color: "green" };
-      window.history.pushState(data,"", data.itemid);
-      update(data);
-      break;
-  }
+  data.itemid = e.target.getAttribute("itemid");
+  update(data.itemid);
 });
 function update(itemid) {
+  if (itemid != null) {
+    switch (itemid) {
+      case "home":
+        data.text = "HOME";
+        data.color = "red";
+        break;
+      case "search":
+        data.text = "SEARCH";
+        data.color = "blue";
+        break;
+      case "likes":
+        data.text = "LIKES";
+        data.color = "pink";
+        break;
+      case "profile":
+        data.text = "ROFILES";
+        data.color = "green";
+        break;
+    }
+    window.history.pushState(data, "", data.itemid);
+  }
   document.querySelector("main").style.backgroundColor = data.color;
   document.querySelector("h1").innerText = data.text;
-  document.querySelectorAll("i").forEach((e)=>e.style.color = "#999");
+  document.querySelectorAll("i").forEach((e) => (e.style.color = "#999"));
   document.querySelector(`i[itemid=${data.itemid}]`).style.color = "black";
 }
+addEventListener("popstate", (event) => {
+    data.itemid= event.state.itemid;
+  data.color= event.state.color;
+  data.text = event.state.text
+//   text: "HOME",
+//     data.text 
+//     data.color = 
+//   console.log(event);
+  update();
+  // data.itemid = event.state.itemid
+  // update(data.itemid)
+});
