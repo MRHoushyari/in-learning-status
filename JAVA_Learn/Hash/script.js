8f0b51f68ae0f4d2ed515ac93a4ff07bcb633341
mohammadreza <MR.Houshyari@Gmail.com>
2022-10-31 08:44:22 +0330

    bug fixed

--- JAVA_Learn/Hash/script.js
+++ JAVA_Learn/Hash/script.js
@@ -1,31 +1,36 @@
-let a = document.querySelector("main");
+let mainPage = document.querySelector("main");
 window.addEventListener("hashchange", () => {
   update(window.location.hash);
 });
 function update(data) {
-  a.style.fontWeight = "bold";
-  a.style.fontSize = "2em";
+  mainPage.style.fontWeight = "bold";
+  mainPage.style.fontSize = "2em";
   document.querySelectorAll("i").forEach((t)=>t.style.color = "#666");
   switch (data) {
     case "#home":
-      a.innerText = "HOME";
-      a.style.backgroundColor = "red";
+      mainPage.innerText = "HOME";
+      mainPage.style.backgroundColor = "red";
       document.getElementById("home").style.color = "black"
       break;
     case "#search":
-      a.innerText = "SEARCH";
-      a.style.backgroundColor = "green";
+      mainPage.innerText = "SEARCH";
+      mainPage.style.backgroundColor = "green";
       document.getElementById("search").style.color = "black"
       break;
     case "#likes":
-      a.innerText = "LIKES";
-      a.style.backgroundColor = "PINK";
+      mainPage.innerText = "LIKES";
+      mainPage.style.backgroundColor = "PINK";
       document.getElementById("likes").style.color = "black"
       break;
     case "#profile":
-      a.innerText = "PROFILE";
-      a.style.backgroundColor = "BLUE";
+      mainPage.innerText = "PROFILE";
+      mainPage.style.backgroundColor = "BLUE";
       document.getElementById("profile").style.color = "black"
       break;
+    default:
+    mainPage.innerText = "HOME";
+    mainPage.style.backgroundColor = "red";
+    document.getElementById("home").style.color = "black"
+    break;
   }
 }
