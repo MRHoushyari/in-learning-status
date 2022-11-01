422a8ffa21301dc4babd83f9d57100d19b9f6d95
mohammadreza <MR.Houshyari@Gmail.com>
2022-11-01 14:50:39 +0330

    Added search option (To Do List)

--- JAVA_Learn/To_do_list/script.js
+++ JAVA_Learn/To_do_list/script.js
@@ -1,7 +1,7 @@
 ///////////////////////////      Usable variable
 let list = document.querySelector("ul");
 let addInput = document.querySelector("#addBox");
-let searchInput = document.querySelector("#searchBox");
+let searchInput = document.querySelector(".searchBox");
 let addBTN = document.querySelector("#addBtn");
 /////////////////////////////       Delete btn event
 list.addEventListener("click", (e) => {
@@ -13,10 +13,44 @@ list.addEventListener("click", (e) => {
 });
 //////////////////////////         Add BTN
 document.querySelector(".addBtn").addEventListener("click", () => {
-  let newItem = document.querySelector("li").cloneNode(true);
-  newItem.querySelector("span").innerText =
-    document.querySelector(".addBox").value;
-  list.appendChild(newItem);
-  document.querySelector(".addBox").value = "";
+  if (document.querySelector(".addBox").value != "") {
+    {
+      if (document.querySelector("ul li") === null)
+        document.querySelector("ul div").remove();
+
+      let newSpan = document.createElement("span");
+      newSpan.innerText = document.querySelector(".addBox").value;
+
+      let newInput = document.createElement("input");
+      newInput.className = "delBtn";
+      newInput.type = "button";
+      newInput.value = "Delete";
+
+      let newItem = document.createElement("li");
+      newItem.appendChild(newSpan);
+      newItem.appendChild(newInput);
+
+      document.querySelector("ul").appendChild(newItem);
+      document.querySelector(".addBox").value = "";
+    }
+  }
 });
 //////////////////////////         Search Box
+searchInput.addEventListener("keyup", (e) => {
+  let listItems = list.querySelectorAll("li");
+  if (listItems.length > 0) {
+    for (let i = 0; i < listItems.length; i++)
+      listItems[i].style.display = "none";
+    for (let i = 0; i < listItems.length; i++) {
+      if (
+        listItems[i]
+          .querySelector("span")
+          .innerText.toLowerCase()
+          .includes(document.querySelector(".searchBox").value.toLowerCase())
+      ) {
+        // console.log("yes");
+        listItems[i].style.display = "flex";
+      }
+    }
+  }
+});
