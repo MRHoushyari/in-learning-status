483aabf66ac2428a3dea61f04e0b5f88b2a05648
mohammadreza <MR.Houshyari@Gmail.com>
2022-11-01 11:56:55 +0330

    Add Button (To Do List)

--- JAVA_Learn/To_do_list/script.js
+++ JAVA_Learn/To_do_list/script.js
@@ -1,13 +1,22 @@
+///////////////////////////      Usable variable
 let list = document.querySelector("ul");
-let addInput = document.querySelector("footer .addBox");
-let searchInput = document.querySelector("header .searchBox");
-let addBTN = document.querySelector("footer .addBtn");
+let addInput = document.querySelector("#addBox");
+let searchInput = document.querySelector("#searchBox");
+let addBTN = document.querySelector("#addBtn");
+/////////////////////////////       Delete btn event
 list.addEventListener("click", (e) => {
-  if ((e.target.className = "delBtn")) e.target.parentNode.remove();
-  {
-    if (document.querySelectorAll("ul li").length == 0) {
-      console.log("yes");
-      list.innerHTML = '<div>The list is empty</div>'
-    }
+  if ((e.target.id = "delBtn")) {
+    e.target.parentNode.remove();
+    if (document.querySelectorAll("ul li").length == 0)
+      list.innerHTML = "<div>The list is empty</div>";
   }
 });
+//////////////////////////         Add BTN
+document.querySelector(".addBtn").addEventListener("click", () => {
+  let newItem = document.querySelector("li").cloneNode(true);
+  newItem.querySelector("span").innerText =
+    document.querySelector(".addBox").value;
+  list.appendChild(newItem);
+  document.querySelector(".addBox").value = "";
+});
+//////////////////////////         Search Box
